6502090bd268bbb79e1bb2fef2ab471953ca88cb
anishshobithps <anish.shobith19@gmail.com>
2026-06-07 00:23:03 +0530

    feat: add stats

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,23 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
+    server: {
+        proxy: {
+            "/stats/script.js": {
+                target: "https://cloud.umami.is",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/stats/, ""),
+            },
+            "/api/send": {
+                target: "https://cloud.umami.is",
+                changeOrigin: true,
+            },
+        },
+    },
 })
